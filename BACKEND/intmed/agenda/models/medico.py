b4bbe9299ca578d6470779841ffafaa5c67b3cc3
wylder.menezes <wylder.menezes@grupoanga.com>
2023-02-04 00:57:49 -0300

    feat: valida dados unicos de cadastro de médicos

--- BACKEND/intmed/agenda/models/medico.py
+++ BACKEND/intmed/agenda/models/medico.py
@@ -1,13 +1,19 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
 class Medico(models.Model):
     nome = models.CharField(max_length=200)
-    crm = models.CharField(max_length=200, blank=False, null=False)
-    email = models.EmailField(max_length=200, blank=False, null=False)
+    crm = models.CharField(max_length=200, blank=False, null=False, unique=True)
+    email = models.EmailField(max_length=200, blank=False, null=False, unique=True)
 
     def __str__(self):
         return self.nome
+
+    def clean (self):
+        if Medico.objects.filter(crm=self.crm).exclude(pk=self.pk).exists():
+            raise ValidationError({'crm': ['Este CRM já está em uso.']})
+
+
 
